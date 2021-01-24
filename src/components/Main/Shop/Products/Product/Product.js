659ab9d959b623b3ab2a14f49cff95148f24292d
Mathieu Istria <mathieu.istria@outlook.fr>
2021-01-24 19:36:58 +0100

    hotfix react crash

--- src/components/Main/Shop/Products/Product/Product.js
+++ src/components/Main/Shop/Products/Product/Product.js
@@ -5,23 +5,29 @@ import { Container, Divider, Loader } from "semantic-ui-react";
 import ProductModal from "../../../../../containers/ProductModal/ProductModal.container";
 import { uniqueKeyID } from "../../../../../utils";
 
-const Product = ({ catId, products,shop }) => {
+const Product = ({ catId, products, shop }) => {
   const location = useLocation();
   const [productLoading, setProductLoading] = useState(false);
   const [isOpenProductModal, setIsOpenProductModal] = useState(false);
   const [productId, setProductId] = useState("");
   const [currentProducts, setCurrentProducts] = useState([]);
-  
 
-  const uniqueProducts = Array.from(
-    new Set(currentProducts.map((product) => product?.subCategoryId))
-  ).map((id) => {
-    return currentProducts.find((product) => product?.subCategoryId === id);
-  });
+  console.log('currentProducts: ', currentProducts);
+
+  let uniqueProducts = [];
+
+  if (currentProducts && currentProducts.length > 0) {
+
+    uniqueProducts = Array.from(new Set(currentProducts.map((product) => product?.subCategoryId))).map((id) => {
+      return currentProducts.find((product) => product?.subCategoryId === id);
+    });
+  }
+
 
   useEffect(() => {
     setProductLoading(true);
-    if (catId !== undefined) {
+    console.log('catId: ', catId);
+    if (!!catId) {
       axios
         .get(`/products/category/${catId}`)
         .then((response) => {
@@ -43,95 +49,96 @@ const Product = ({ catId, products,shop }) => {
         productLoading ? (
           <Loader active={productLoading} />
         ) : (
-          //mettre tout ca dans un composant dans le store management
-          currentProducts.map((product) => (
-            <>
-              <Container textAlign="left">
-                <div
-                  key={uniqueKeyID()}
-                  tabIndex={0}
-                  onClick={(e) => handleClick(product?._id)}
-                >
-                  <div className="first">
-                    <div className="second">
-                      <div className="third">
-                        <div className="fourth">
-                          <h4>
-                            <div className="product__title">
-                              {product?.name}
-                            </div>
-                          </h4>
+
+            //mettre tout ca dans un composant dans le store management
+            currentProducts && currentProducts.length > 0 && currentProducts.map((product) => (
+              <>
+                <Container textAlign="left">
+                  <div
+                    key={uniqueKeyID()}
+                    tabIndex={0}
+                    onClick={(e) => handleClick(product?._id)}
+                  >
+                    <div className="first">
+                      <div className="second">
+                        <div className="third">
+                          <div className="fourth">
+                            <h4>
+                              <div className="product__title">
+                                {product?.name}
+                              </div>
+                            </h4>
+                          </div>
+                          <div className="product__description">
+                            <div>{product?.description}</div>
+                          </div>
+                          <div className="product__description">
+                            {product.options &&
+                              Object.keys(product.options).map((option) => (
+                                <p>
+                                  {option}: {product.options[option]}
+                                </p>
+                              ))}
+                          </div>
+                          <div className="product__price">
+                            <div>{product?.price} €</div>
+                          </div>
                         </div>
-                        <div className="product__description">
-                          <div>{product?.description}</div>
+                        <div className="product__img">
+                          <picture>
+                            <img
+                              alt={product?.name}
+                              src={product?.urlImage}
+                              aria-hidden="true"
+                            />
+                          </picture>
                         </div>
-                        <div className="product__description">
-                          {product.options &&
-                            Object.keys(product.options).map((option) => (
-                              <p>
-                                {option}: {product.options[option]}
-                              </p>
-                            ))}
-                        </div>
-                        <div className="product__price">
-                          <div>{product?.price} €</div>
-                        </div>
-                      </div>
-                      <div className="product__img">
-                        <picture>
-                          <img
-                            alt={product?.name}
-                            src={product?.urlImage}
-                            aria-hidden="true"
-                          />
-                        </picture>
                       </div>
                     </div>
                   </div>
-                </div>
-              </Container>
-              <Divider hidden />
-            </>
-          ))
-        )
+                </Container>
+                <Divider hidden />
+              </>
+            ))            
+          )
       ) : productLoading ? (
         <Loader active={productLoading} />
       ) : (
-        uniqueProducts.map((product) => (
-          <div
-            key={uniqueKeyID()}
-            tabIndex={0}
-            onClick={(e) => handleClick(product?._id)}
-          >
-            <div className="first">
-              <div className="second">
-                <div className="third">
-                  <div className="fourth">
-                    <h4>
-                      <div className="product__title">{product?.name}</div>
-                    </h4>
+            uniqueProducts.map((product) => (
+              <div
+                key={uniqueKeyID()}
+                tabIndex={0}
+                onClick={(e) => handleClick(product?._id)}
+              >
+                <div className="first">
+                  <div className="second">
+                    <div className="third">
+                      <div className="fourth">
+                        <h4>
+                          <div className="product__title">{product?.name}</div>
+                        </h4>
+                      </div>
+                      <div className="product__description">
+                        <div>{product?.description}</div>
+                      </div>
+                      <div className="product__price">
+                        <div>{product?.price} €</div>
+                      </div>
+                    </div>
+                    <div className="product__img">
+                      <picture>
+                        <img
+                          alt={product?.name}
+                          src={product?.urlImage}
+                          aria-hidden="true"
+                        />
+                      </picture>
+                    </div>
                   </div>
-                  <div className="product__description">
-                    <div>{product?.description}</div>
-                  </div>
-                  <div className="product__price">
-                    <div>{product?.price} €</div>
-                  </div>
-                </div>
-                <div className="product__img">
-                  <picture>
-                    <img
-                      alt={product?.name}
-                      src={product?.urlImage}
-                      aria-hidden="true"
-                    />
-                  </picture>
                 </div>
               </div>
-            </div>
-          </div>
-        ))
-      )}
+            ))
+          )}
       <ProductModal
         shop={shop}
         isOpenProductModal={isOpenProductModal}
